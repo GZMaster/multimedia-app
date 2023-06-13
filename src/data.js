833b5fab66086012c87ee81037b1c0ea3e3844bb
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-06-13 16:13:33 +0100

    quest completed

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		id: 1,
+		name: "#foryou",
+		type: "video",
+		path: "/file-server/#foryou.mp4",
+	},
+	{
+		id: 2,
+		name: "town",
+		type: "audio",
+		path: "/file-server/town.mp3",
+	},
+	{
+		id: 3,
+		name: "Art of War",
+		type: "document",
+		path: "/file-server/Art of War.pdf",
+	},
+	{
+		id: 4,
+		name: "clay-banks",
+		type: "image",
+		path: "/file-server/clay-banks.jpg",
+	},
+	{
+		id: 5,
+		name: "keegan-houser",
+		type: "image",
+		path: "/file-server/keegan-houser.jpg",
+	},
+	{
+		id: 6,
+		name: "tembinkosi-sikupela",
+		type: "image",
+		path: "/file-server/tembinkosi-sikupela.jpg",
+	},
+];
